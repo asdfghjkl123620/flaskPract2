7273913535c47531536de520fc3fed3efaea2791
zxcv5608 <asdfghjkl123620@gmail.com>
2020-11-03 17:06:56 +0800

    register,login for detail complete features

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    #SQLite資料庫的路徑os.environ.get拿取環境變數:資料庫的位置
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
